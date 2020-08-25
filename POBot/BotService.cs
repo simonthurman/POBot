e5dc2199c744b33a16e1f3c899c9e9ab08e0edae
Simon Thurman <simonthurman@hotmail.com>
2020-08-25 12:05:25 +0100

    Add project files.

---
+++ POBot/BotService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.Bot.Builder.AI.Luis;
+using Microsoft.Bot.Builder.AI.QnA;
+using Microsoft.Extensions.Configuration;
+
+namespace POBot
+{
+    public class BotService : IBotService
+    {
+        public BotService(IConfiguration configuration)
+        {
+            var luisApplication = new LuisApplication(
+                configuration["LuisAppId"],
+                configuration["LuisAppKey"],
+                $"https://{configuration["LuisAPIHostName"]}.api.cognitive.microsoft.com");
+
+            var recognizerOptions = new LuisRecognizerOptionsV2(luisApplication)
+            {
+                IncludeAPIResults = true,
+                PredictionOptions = new LuisPredictionOptions()
+                {
+                    IncludeAllIntents = true,
+                    IncludeInstanceData = true
+                }
+            };
+
+            Dispatch = new LuisRecognizer(recognizerOptions);
+
+            POHowToQnA = new QnAMaker(new QnAMakerEndpoint
+            {
+                KnowledgeBaseId = configuration["QnAKnowlegebaseId"],
+                EndpointKey = configuration["QnAEnpointKey"],
+                Host = configuration["QnAEndpointHostName"]
+            });
+        }
+        public LuisRecognizer Dispatch { get; private set; }
+
+        public QnAMaker POHowToQnA { get; private set; }
+    }
+
+
+
+}
